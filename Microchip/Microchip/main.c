ada159cd9a95fc289836e2c191f1f00116398dd5
rmanuelx5 <reyesmanuelx5@gmail.com>
2024-05-28 21:24:47 -0300

    Agrego archivos en blanco de Proteus y Microchip

---
+++ Microchip/Microchip/main.c
@@ -0,0 +1,18 @@
+/*
+ * Microchip.c
+ *
+ * Created: 28/5/2024 21:19:47
+ * Author : manuc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
