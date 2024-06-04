6d1961cf7856027670df1eae8134ce1b72fc71b9
rmanuelx5 <reyesmanuelx5@gmail.com>
2024-06-04 17:55:25 -0300

    Avance en codigo y pruebas

--- Microchip/Microchip/main.c
+++ Microchip/Microchip/main.c
@@ -28,7 +28,10 @@ int main(void)
 #define BR9600 (0x67)	// 0x67=103 configura BAUDRATE=9600@16MHz
 
 //mensajes de bienvenida y despedida
-char msg1[] = "TEMP: %02d °C HUM: %02d\% FECHA: %02d/%02d/%02d HORA:%02d:%02d:%03d\r\n";//temp, hum, dd/mm/aa, hh/mm/sss
+//char msg1[] = "TEMP: %02d °C HUM: %02d\% FECHA: %02d/%02d/%02d HORA:%02d:%02d:%03d\r\n";//temp, hum, dd/mm/aa, hh/mm/sss
+char  msg1[] = "prueba";
+uint8_t activo= 0; //usado como variable de control
+volatile char RX_Buffer=0;
 
 
 int main(void)	
@@ -45,22 +48,31 @@ int main(void)
 		dato = SerialPort_Recive_Data();
 
 		// Si presionan 's' se termina o reanuda
-		if( dato == 's')
+		if( dato == 's' || dato == 'S' )
 		{
-			// bool activo o desactivo
+			activo = ~activo;
+			// toggle activo
 		}	
-		else
+		/*else
 		{				
 			SerialPort_Wait_For_TX_Buffer_Free(); // Espero a que el canal de transmisión este libre (bloqueante)
 			//cada 2 segundos mando a terminal
+			
 			SerialPort_Send_String(msg1);    
-		}
+		}*/
 	}
 	return 0;
+
 }
+
 // Rutina de Servicio de Interrupción de Byte Recibido
 ISR(USART_RX_vect){
-	if (activo)
-		imprimirMensaje();
+	if (activo){
+		//imprimirMensaje();
+		SerialPort_Wait_For_TX_Buffer_Free(); // Espero a que el canal de transmisión este libre (bloqueante)
+		//cada 2 segundos mando a terminal
+		
+		SerialPort_Send_String(msg1);
+	}
 	RX_Buffer = UDR0; //la lectura del UDR borra flag RXC
 }
